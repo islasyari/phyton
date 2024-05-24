cd52cced8003d1283f3fd622bcb3500accfd5490
islasyari <79239151+islasyari@users.noreply.github.com>
2024-05-24 17:12:48 -0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,9 @@
+# Knock-Knock Joke Program
+print("Knock, knock.")  
+print("Who's there?")  
+print("A broken pencil.")  
+print("A broken pencil who?")  
+print("Never mind it's pointless!")  
+
+# both codes are the same results different set up 
+print("Knock, knock.\nWho's there?\nA broken pencil.\nA broken pencil who?\nNever mind it's pointless!")
