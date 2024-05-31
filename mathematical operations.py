2042470dd5a8269bae83c4b64d579747150dbe8b
islasyari <79239151+islasyari@users.noreply.github.com>
2024-05-30 20:06:47 -0500

    Add files via upload

---
+++ mathematical operations.py
@@ -0,0 +1,6 @@
+# Demonstration of formatting numbers into a string
+length = 2
+width = 2
+area = length * width
+
+print("The area of the rectangle with a length of " + "" + str(length) + ", and a width of " + str(width) + " is", area) 
