0873057bfe30b1787d87bf50a090d66e5b7cf32e
islasyari <79239151+islasyari@users.noreply.github.com>
2024-06-02 21:43:09 -0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,12 @@
+#calculating total budget vs spending
+Budget = float(input(" Enter a whole number (monthly goal): "))
+Housing = float(input("Enter a monthly amount (rent or mortgage): "))
+Utilities = float(input("Enter a monthly amount: "))
+Groceries = float(input("Enter a monthly amount: "))
+Transportation = float(input("Enter a monthly amount: "))
+Health_Care = float(input("Enter a monthly amount: "))
+Personal_Care = float(input("Enter a monthly amount: "))
+Clothing = float(input("Enter a monthly amount: "))
+Debt = float(input("Enter a whole number: "))
+Calculate the percentage of the total budget spent in each category.
+len(Housing-Debt)
