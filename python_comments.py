852da11f490ebb618968b5b2504cc59117b93df6
islasyari <79239151+islasyari@users.noreply.github.com>
2024-06-02 21:38:40 -0500

    Add files via upload

---
+++ python_comments.py
@@ -0,0 +1,9 @@
+# This program calculates the mpg.
+# our variables we are using is our tank size and distance.
+tank_size = 15
+distance = 600
+
+mpg = distance / tank_size #we use / to dive the distance and size tank
+print("Miles per gallon:", mpg)
+#here we should also print "Goodbye", but a programmer didn't have time to write any code
+#this is the end of the program that computes the miles per hour
