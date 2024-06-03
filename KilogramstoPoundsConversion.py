1f3245a77961a863d5df539d2c4f684e5cb41055
islasyari <79239151+islasyari@users.noreply.github.com>
2024-06-02 21:40:02 -0500

    Add files via upload

---
+++ KilogramstoPoundsConversion.py
@@ -0,0 +1,18 @@
+# Convert kilograms to pounds
+Conversion_Factor= 2.20462
+Kilograms= 3
+Pounds= Kilograms * Conversion_Factor
+print("pounds =", Pounds)
+
+Conversion_Factor /= 3 * Pounds
+print("3 kilograms is equal to",Pounds)
+
+Conversion_Factor *= 3
+print("3 kilograms=", Pounds)
+
+pounds_to_kilograms = 1
+Kilograms_to_pounds = 0.454
+pound = 2
+print(Kilograms_to_pounds, "kilograms is", round(pounds_to_kilograms, 2), "pound")
+
+print(Kilograms * Conversion_Factor)
