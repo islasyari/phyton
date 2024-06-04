b3fe74a1c2ec60426b70dc0826b24a9e8a93466e
islasyari <79239151+islasyari@users.noreply.github.com>
2024-06-04 11:51:26 -0500

    Add files via upload

---
+++ making_decisions.py
@@ -0,0 +1,25 @@
+age = int(input("How old are you?: ")) # enter your age
+
+
+if age >= 16:
+    print ("You are old enough to drive.")
+else:
+    print ("You are not old enough to drive.") 
+
+if age >= 18:
+    print ("You can vote.")
+else:
+    print ("You can't vote.")
+
+if age >= 21:
+    print ("You can buy alcohol legally.")
+else:
+    print ("You can't buy alcohol legally.")
+
+if age >= 60:
+    print ("You are eligible for senior discount.")
+else:
+    print ("You are not eligible for senior discount.")
+
+ 
+
