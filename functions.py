42d42c515940882d5fbc1189795bb58a6c466324
islasyari <79239151+islasyari@users.noreply.github.com>
2024-06-15 17:04:36 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,11 @@
+def square(side): # Define the square function.
+    area = side * side # Calculate the area of a square.
+    print ("The area of the square is " + str(area) + " square units. ")
+
+def circle(radius): # define the circle function.
+    area = 3.14 * radius * radius #calculate the area of a circle. 
+    print ("The area of a circle is " + str(area) + " square units. ")
+
+# Test the functions.
+square (4)
+circle (5)
