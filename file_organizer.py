8bee0dd89f960ce72a66c7140624f825514fc55e
islasyari <79239151+islasyari@users.noreply.github.com>
2024-07-21 19:41:35 -0500

    Add files via upload

---
+++ file_organizer.py
@@ -0,0 +1,9 @@
+import os
+
+# Create the MyFiles directory
+os.mkdir("MyFiles")
+
+# Create the Docs, Images, and Videos subdirectories
+os.mkdir("MyFiles/Docs")
+os.mkdir("MyFiles/Images")
+os.mkdir("MyFiles/Videos")
